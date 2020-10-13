df89c72c7ec1dc625f19456c007bf80db60f535c
MaryGrace Blair <mblair021@gmail.com>
2020-10-13 13:34:51 -0500

    added initial route within the routes file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express'),
+const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
 app.listen(port);
 
 console.log('RxSavings RESTful API server started on: ' + port);
+
+//import and register routes
+const routes = require('./api/routes/routes');
+routes(app);
 
